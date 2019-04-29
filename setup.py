3c44dadd83f415a7394ea1c0b565191caf895471
mathraim <artmath1998@gmail.com>
2019-04-29 09:53:22 -0700

    initial commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deepmath",
-    version="0.0.2",
+    version="0.0.1",
     author="Raiymbek",
     author_email="raimogold1998@gmail.com",
     description="A small example package",
