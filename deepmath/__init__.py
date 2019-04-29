3bda7b25ca97dfe2706e5af27cce96f95c69a086
mathraim <artmath1998@gmail.com>
2019-04-29 10:48:07 -0700

    initial commit

--- deepmath/__init__.py
+++ deepmath/__init__.py
@@ -1,1 +1,14 @@
 name = "deepmath"
+from deepmath.base_classes.layer import Layer
+from deepmath.base_classes.network import Network
+from deepmath.base_classes.optimizer import Optimizer
+from deepmath.layers.dense import Dense
+from deepmath.layers.activation import Activation
+from deepmath.layers.activation_functions.lrelu import LRelu
+from deepmath.layers.activation_functions.relu import Relu
+from deepmath.layers.activation_functions.sigmoid import Sigmoid
+from deepmath.layers.activation_functions.tanh import Tanh
+from deepmath.optimizers.adam import Adam
+from deepmath.optimizers.momentum import Momentum
+from deepmath.optimizers.rmsprop import RMSprop
+from deepmath.optimizers.sgd import SGD
