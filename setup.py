fa65d5c47aa0b4f54c2e2a7868b74ee5477f8d03
mathraim <artmath1998@gmail.com>
2019-04-30 23:48:29 -0700

    initial commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deepmath",
-    version="0.0.4",
+    version="0.2",
     author="Raiymbek",
     author_email="artmath1998@gmail.com",
     description="A machine learning library for educational purposes ",
