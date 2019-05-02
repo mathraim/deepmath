4f095dc312298a8d5c474e3314cfc8a50cfd6471
mathraim <artmath1998@gmail.com>
2019-05-01 22:31:51 -0700

    initial commit

---
+++ deepmath/optimizers/__init__.py
@@ -0,0 +1,4 @@
+from deepmath.optimizers.sgd import SGD
+from deepmath.optimizers.momentum import Momentum
+from deepmath.optimizers.rmsprop import RMSprop
+from deepmath.optimizers.adam import Adam
